a63049ce54a0da23d2e607f566ebfd24b46722c7
Qamar1998 <QuqaQamar-1998@hotmail.com>
2020-01-06 14:56:21 +0200

    ghgh

--- src/app/layouts/admin-layout/material/material.module.ts
+++ src/app/layouts/admin-layout/material/material.module.ts
@@ -4,10 +4,22 @@ MatDialogModule,
 
 } from '@angular/material'
 
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCardModule} from '@angular/material';
+
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 const MaterialComponents=[
   MatButtonModule,
   MatDialogModule,
-
+  MatProgressSpinnerModule,
+  MatCardModule,
+  MatProgressBarModule,
+  MatInputModule,
+  MatFormFieldModule
 ]
 @NgModule({
   imports: [MaterialComponents],
